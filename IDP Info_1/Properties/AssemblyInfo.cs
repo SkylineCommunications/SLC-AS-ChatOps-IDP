c5b27cd935eb1d099fa3bbdf6b1e4df76f8d6e9b
Joey Vanhalst <joey.vanhalst@skyline.be>
2023-05-31 10:42:48 +0200

    initial solution

---
+++ IDP Info_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("IDP Info_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("IDP Info_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("D5D0C853-5E85-4A17-B16E-19553FA9F15D")]
+[assembly: AssemblyVersion("1.0.0.0")]
